798374ff268e0ae696c4b7ddebe4bb6c74d189d5
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2021-04-01 14:16:57 +0300

    Add files via upload

---
+++ 1180505018/1180505018/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace _1180505018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEksen = new System.Windows.Forms.Button();
+            this.btnDondur = new System.Windows.Forms.Button();
+            this.txtDondur = new System.Windows.Forms.TextBox();
+            this.btnYansıt = new System.Windows.Forms.Button();
+            this.btnCiz = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEksen
+            // 
+            this.btnEksen.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnEksen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEksen.ForeColor = System.Drawing.Color.Maroon;
+            this.btnEksen.Location = new System.Drawing.Point(12, 35);
+            this.btnEksen.Name = "btnEksen";
+            this.btnEksen.Size = new System.Drawing.Size(153, 61);
+            this.btnEksen.TabIndex = 0;
+            this.btnEksen.Text = "Eksenleri Çiz";
+            this.btnEksen.UseVisualStyleBackColor = false;
+            this.btnEksen.Click += new System.EventHandler(this.btnEksen_Click);
+            // 
+            // btnDondur
+            // 
+            this.btnDondur.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnDondur.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDondur.ForeColor = System.Drawing.Color.Maroon;
+            this.btnDondur.Location = new System.Drawing.Point(12, 185);
+            this.btnDondur.Name = "btnDondur";
+            this.btnDondur.Size = new System.Drawing.Size(101, 44);
+            this.btnDondur.TabIndex = 1;
+            this.btnDondur.Text = "Döndür";
+            this.btnDondur.UseVisualStyleBackColor = false;
+            this.btnDondur.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDondur
+            // 
+            this.txtDondur.Location = new System.Drawing.Point(119, 199);
+            this.txtDondur.Name = "txtDondur";
+            this.txtDondur.Size = new System.Drawing.Size(48, 20);
+            this.txtDondur.TabIndex = 2;
+            // 
+            // btnYansıt
+            // 
+            this.btnYansıt.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnYansıt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYansıt.ForeColor = System.Drawing.Color.Maroon;
+            this.btnYansıt.Location = new System.Drawing.Point(12, 246);
+            this.btnYansıt.Name = "btnYansıt";
+            this.btnYansıt.Size = new System.Drawing.Size(101, 44);
+            this.btnYansıt.TabIndex = 3;
+            this.btnYansıt.Text = "Yansıt";
+            this.btnYansıt.UseVisualStyleBackColor = false;
+            this.btnYansıt.Click += new System.EventHandler(this.btnYansıt_Click);
+            // 
+            // btnCiz
+            // 
+            this.btnCiz.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnCiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCiz.ForeColor = System.Drawing.Color.Maroon;
+            this.btnCiz.Location = new System.Drawing.Point(12, 121);
+            this.btnCiz.Name = "btnCiz";
+            this.btnCiz.Size = new System.Drawing.Size(101, 44);
+            this.btnCiz.TabIndex = 4;
+            this.btnCiz.Text = "Çiz";
+            this.btnCiz.UseVisualStyleBackColor = false;
+            this.btnCiz.Click += new System.EventHandler(this.btnCiz_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnClear.ForeColor = System.Drawing.Color.Maroon;
+            this.btnClear.Location = new System.Drawing.Point(12, 321);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(101, 44);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCiz);
+            this.Controls.Add(this.btnYansıt);
+            this.Controls.Add(this.txtDondur);
+            this.Controls.Add(this.btnDondur);
+            this.Controls.Add(this.btnEksen);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEksen;
+        private System.Windows.Forms.Button btnDondur;
+        private System.Windows.Forms.TextBox txtDondur;
+        private System.Windows.Forms.Button btnYansıt;
+        private System.Windows.Forms.Button btnCiz;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ 1180505018/1180505018/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1180505018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Drawing.Graphics Cizgi;
+
+
+        //kordinat sisteminde çizim yapmak için tanımlı bir kütüphane kullanıyoruz
+        Pen Klm = new Pen(Color.Red, 2);
+
+        //işlem yappılırken kolaylık olması için global bir dizi tanımlıyoruz
+        Double[,] XYeni = new Double[3, 3];
+
+        //üçgenin noktalarını veritoruz
+        Double[,] XCisimNokta = new Double[3, 3]{ {2,3,1},
+                                                  {7,3,1},
+                                                  {2,6,1} };
+
+        //burda döndürme işlemi için döndürme dizisini tanımlıyor fonksiyon üzerinde açıları veriyoruz
+        Double[,] Tdondur = new Double[3, 3]{ {1, 0, 0},
+                                              {0, 1, 0},
+                                              {0, 0, 1} };
+
+        // x eksenin de yansıtma yapmak için y eksenine (-) değer veriyoruz 
+        Double[,] Tyansit = new double[3, 3]{ { 1,  0, 0 },
+                                              { 0, -1, 0 },
+                                              { 0,  0, 1 } };
+
+        //üçgeni kordinat sistemine sığdırmak için ölçülendiriyoruz
+        static double sıgdır(double x)
+        {
+            return x * (0.1);
+        }
+        int i, j, k;
+
+
+        //ekranda kordinat sisteminin çizileceği konumu belirliyoruz
+        private int KoordinatHesaplaX(Double geciciX)
+        {
+            return Convert.ToInt32(300 + 250 + (200 * geciciX));
+        }
+
+        private int KoordinatHesaplaY(Double geciciY)
+        {
+            return Convert.ToInt32(250 + (-200 * geciciY));
+        }
+
+        //Çizilecek üögenin temel noktalarını tanımlıyoruz
+        private void Ciz()
+        {
+            Cizgi = this.CreateGraphics();
+
+            Klm.Color = Color.Yellow;
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[0, 0])), KoordinatHesaplaY(sıgdır(XYeni[0, 1])), KoordinatHesaplaX(sıgdır(XYeni[1, 0])), KoordinatHesaplaY(sıgdır(XYeni[1, 1])));
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[1, 0])), KoordinatHesaplaY(sıgdır(XYeni[1, 1])), KoordinatHesaplaX(sıgdır(XYeni[2, 0])), KoordinatHesaplaY(sıgdır(XYeni[2, 1])));
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[2, 0])), KoordinatHesaplaY(sıgdır(XYeni[2, 1])), KoordinatHesaplaX(sıgdır(XYeni[0, 0])), KoordinatHesaplaY(sıgdır(XYeni[0, 1])));
+            Cizgi.Dispose();
+        }
+
+
+        //kordinat sisteminin çizimini butona aktardım kordinat sistemini çiziyor
+        private void btnEksen_Click(object sender, EventArgs e)
+        {
+            Cizgi = this.CreateGraphics();
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(-1), KoordinatHesaplaY(0), KoordinatHesaplaX(1), KoordinatHesaplaY(0));
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(0), KoordinatHesaplaY(-1), KoordinatHesaplaX(0), KoordinatHesaplaY(1));
+            Cizgi.Dispose();
+        }
+
+        //çiz butonunu tanımlıyoruz üçgenin değerlerini xyeni dizisine aktarıp çizim işlemini gerçekleştiriyoruz
+        private void btnCiz_Click(object sender, EventArgs e)
+        {
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    XYeni[i, j] = 0;
+
+                    XYeni[i, j] = XCisimNokta[i, j];
+                }
+            }
+
+            Ciz();
+        }
+
+        //temizleme işlemi
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //döndürme işlemini gerçekleştiriyor
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double[,] Gecici = new Double[3, 3];
+            Double teta = 30;
+
+            //alınan derece randyana çevrilip döndürme matrisinde yerlerine yerleştiriliyor
+            teta = Convert.ToDouble(txtDondur.Text);
+            Tdondur[0, 0] = Math.Cos(Math.PI * teta / 180.0);
+            Tdondur[1, 1] = Tdondur[0, 0];
+
+            Tdondur[1, 0] = Math.Sin(Math.PI * teta / 180.0);
+            Tdondur[0, 1] = (-1) * Tdondur[1, 0];
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Gecici[i, j] = 0;
+                    for (k = 0; k < 3; k++)
+                    {
+                        Gecici[i, j] += XCisimNokta[i, k] * Tdondur[k, j];
+                    }
+                }
+            }
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    XYeni[i, j] = 0;
+
+                    XYeni[i, j] = Gecici[i, j];
+                }
+            }
+
+            Klm.Color = Color.Purple;
+            Ciz();
+            Cizgi = this.CreateGraphics();
+            Klm.Color = Color.Purple;
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[0, 0])), KoordinatHesaplaY(sıgdır(XYeni[0, 1])), KoordinatHesaplaX(sıgdır(XYeni[1, 0])), KoordinatHesaplaY(sıgdır(XYeni[1, 1])));
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[1, 0])), KoordinatHesaplaY(sıgdır(XYeni[1, 1])), KoordinatHesaplaX(sıgdır(XYeni[2, 0])), KoordinatHesaplaY(sıgdır(XYeni[2, 1])));
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[2, 0])), KoordinatHesaplaY(sıgdır(XYeni[2, 1])), KoordinatHesaplaX(sıgdır(XYeni[0, 0])), KoordinatHesaplaY(sıgdır(XYeni[0, 1])));
+            Cizgi.Dispose();
+        }
+
+
+        //x eksenine göre yansıtma işlemi gerçekleştiriyoruz
+        private void btnYansıt_Click(object sender, EventArgs e)
+        {
+            Double[,] Gecici = new Double[3, 3];
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Gecici[i, j] = 0;
+                    for (k = 0; k < 3; k++)
+                    {
+                        Gecici[i, j] += XCisimNokta[i, k] * Tyansit[k, j];
+                    }
+                }
+            }
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    XYeni[i, j] = 0;
+
+                    XYeni[i, j] = Gecici[i, j];
+                }
+            }
+
+            Klm.Color = Color.Blue;
+            Ciz();
+            
+            Cizgi = this.CreateGraphics();
+            Klm.Color = Color.Blue;
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[0, 0])), KoordinatHesaplaY(sıgdır(XYeni[0, 1])), KoordinatHesaplaX(sıgdır(XYeni[1, 0])), KoordinatHesaplaY(sıgdır(XYeni[1, 1])));
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[1, 0])), KoordinatHesaplaY(sıgdır(XYeni[1, 1])), KoordinatHesaplaX(sıgdır(XYeni[2, 0])), KoordinatHesaplaY(sıgdır(XYeni[2, 1])));
+            Cizgi.DrawLine(Klm, KoordinatHesaplaX(sıgdır(XYeni[2, 0])), KoordinatHesaplaY(sıgdır(XYeni[2, 1])), KoordinatHesaplaX(sıgdır(XYeni[0, 0])), KoordinatHesaplaY(sıgdır(XYeni[0, 1])));
+            Cizgi.Dispose();
+
+        }
+
+    }
+}
